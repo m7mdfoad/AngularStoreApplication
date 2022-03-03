c4dc1de7c627335cb803d60b29996a931892abb2
Muhammad abu alhija <a.m7md.f@gmail.com>
2022-03-03 12:46:20 +0200

    3-3-2022

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,50 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule, HammerModule} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { LoginComponent } from './login/login.component';
+import { SalesComponent } from './sales/sales.component';
+import { StockComponent } from './stock/stock.component';
+import { PartnersComponent } from './partners/partners.component';
+import { OrdersComponent } from './orders/orders.component';
+import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { TitleComponent } from './layout/title/title.component';
+import { ToolbarComponent } from './layout/toolbar/toolbar.component';
+import { ContentComponent } from './layout/content/content.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import {HttpClientModule} from "@angular/common/http";
+import { EmployeesComponent } from './layout/content/tables/employees/employees.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent,
+    LoginComponent,
+    SalesComponent,
+    StockComponent,
+    PartnersComponent,
+    OrdersComponent,
+    ProductsComponent,
+    HomeComponent,
+    TitleComponent,
+    ToolbarComponent,
+    ContentComponent,
+    FooterComponent,
+    EmployeesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HammerModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
